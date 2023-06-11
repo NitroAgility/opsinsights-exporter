ba52cc5bdde6a57f42c29fa72bf0ea67467d437d
Nicola Gallo <nicolagmt@hotmail.com>
2023-06-11 22:48:39 +0200

    Updated README

--- pkg/internal/configs/doc.go
+++ pkg/internal/configs/doc.go
@@ -14,4 +14,5 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package core
+// Package configs implement the applicative configuration layer.
+package configs
